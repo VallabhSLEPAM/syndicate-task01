117535ba3a93a846a41f14b803cdfd7bffec8902
VallabhSLEPAM <vallabhshivahar_lakade@epam.com>
2024-12-10 18:30:26 +0530

    feat: added lambda URL config

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -4,6 +4,13 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+import com.syndicate.deployment.model.DeploymentRuntime;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,15 +20,38 @@ import java.util.Map;
 	roleName = "hello_world-role",
 	isPublishVersion = true,
 	aliasName = "${lambdas_alias_name}",
+	runtime = DeploymentRuntime.JAVA11,
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
+@LambdaUrlConfig(
+        authType = AuthType.NONE,
+        invokeMode = InvokeMode.BUFFERED
+)
+public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
-		return resultMap;
+	@Override
+    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
+        // Extract HTTP method and path
+        String path = event.getRawPath(); // Path of the HTTP request
+        String method = event.getRequestContext().getHttp().getMethod(); // HTTP method
+
+        // Handle /hello GET request
+        if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
+            return APIGatewayV2HTTPResponse.builder()
+                    .withStatusCode(200)
+                    .withBody("{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}")
+                    .build();
+        }
+
+        // Handle other requests with a 400 Bad Request response
+        String errorMessage = String.format(
+                "{\"statusCode\": 400, \"message\": \"Bad request syntax or unsupported method. Request path: %s. HTTP method: %s\"}",
+                path, method
+        );
+        return APIGatewayV2HTTPResponse.builder()
+                .withStatusCode(400)
+                .withBody(errorMessage)
+                .build();
+		
 	}
 }
