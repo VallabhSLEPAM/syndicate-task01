9d15ed47d3d3b07cc959efa7b6aee136808af07a
VallabhSLEPAM <vallabhshivahar_lakade@epam.com>
2024-12-20 11:21:25 +0530

    feat: added annotations for mapping lambda to sqs and sns

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -4,6 +4,9 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.annotations.events.SnsEventSource;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.ResourceType;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +17,11 @@ import java.util.Map;
 	isPublishVersion = true,
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@SnsEventSource(targetTopic = "lambda_topic")
+@DependsOn(
+        name = "lambda_topic",
+        resourceType = ResourceType.SNS_TOPIC//"sns_topic"
 )
 public class SnsHandler implements RequestHandler<Object, Map<String, Object>> {
 
--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -4,6 +4,9 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.ResourceType;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +17,14 @@ import java.util.Map;
 	isPublishVersion = true,
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@SqsTriggerEventSource(
+	targetQueue = "async_queue",
+	batchSize = 1
+)
+@DependsOn(
+        name = "async_queue",
+        resourceType = ResourceType.SQS_QUEUE
 )
 public class SqsHandler implements RequestHandler<Object, Map<String, Object>> {
 
